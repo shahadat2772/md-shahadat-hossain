102382c90a70b579cccedef3f1c1755f19fb643a
shahadat2772 <shahadat2772@gmailcom>
2022-06-09 19:39:43 +0600

    added the banner section

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,34 @@
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import laptop from "../../../assets/images/laptop44.png";
+
+const Banner = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content md:gap-6 flex-col lg:flex-row-reverse">
+        <img alt="" src={laptop} className="md:max-w-[470px]" />
+        <div className="max-w-xl">
+          <h1 className="md:text-[80px] text-[45px] md:leading-[78px] leading-[53px]">
+            Hello, <br /> I'm Shahadat, <br />
+            Web Developer.
+          </h1>
+          <Link
+            to={"/projects"}
+            className="btn btn-primary btn-outline mt-8 ml-1"
+          >
+            Projects
+          </Link>
+          <button className="btn btn-primary btn-outline ml-2">
+            <a href="https://drive.google.com/u/0/uc?id=1RpnuRcI_0fjmTDayBN2-wxEeQbaQoEVZ&export=download">
+              Resume <FontAwesomeIcon icon={faDownload} />
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,57 +1,66 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   const menuItems = (
     <>
       <li>
-        <a>Item 1</a>
+        <NavLink to={"/home"}>Home</NavLink>
       </li>
       <li>
-        <a>Item 3</a>
+        <NavLink to={"/projects"}>Projects</NavLink>
       </li>
       <li>
-        <a>Item 3</a>
+        <NavLink to={"/contact"}>Contact</NavLink>
       </li>
     </>
   );
 
   return (
-    <div class="navbar bg-base-100">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </label>
           <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             {menuItems}
           </ul>
         </div>
-        <a href="" class="btn btn-ghost normal-case text-xl">
+        <a href="iu" className="btn btn-ghost normal-case text-xl">
           ShahadatTheDev
         </a>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">{menuItems}</ul>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
-      <div class="navbar-end">
-        <a class="btn btn-outline btn-sm">Get Resume</a>
-      </div>
+      {/* <div className="navbar-end">
+        <a
+          href="https://drive.google.com/u/0/uc?id=1RpnuRcI_0fjmTDayBN2-wxEeQbaQoEVZ&export=download"
+          className="btn btn-outline btn-sm"
+        >
+          Resume <FontAwesomeIcon className="pl-1" icon={faDownload} />
+        </a>
+      </div> */}
     </div>
   );
 };
